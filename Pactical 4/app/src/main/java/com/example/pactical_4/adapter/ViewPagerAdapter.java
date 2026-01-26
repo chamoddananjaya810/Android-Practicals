a7ab8dd9a8d90c2f341e30b8a979a64d060aeb1c
chamoddananjaya810 <chamoddhananjaya76@gmail.com>
2026-01-26 14:11:42 +0530

    feat(ui)implement signin and sign up

---
+++ Pactical 4/app/src/main/java/com/example/pactical_4/adapter/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.pactical_4.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.pactical_4.fragment.SignInFragment;
+import com.example.pactical_4.fragment.SignUpFragment;
+
+public class ViewPagerAdapter  extends FragmentStateAdapter {
+
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position ==1){
+            return new SignInFragment();
+        }
+        return new SignUpFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
